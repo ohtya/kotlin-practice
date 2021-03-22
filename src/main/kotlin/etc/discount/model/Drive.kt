309727f55b948cec5f0a965ad1c834c58e18541f
ohtya <48324058+ohtya@users.noreply.github.com>
2021-03-23 00:50:36 +0900

    doc: 走行データに kdoc を追加

--- src/main/kotlin/etc/discount/model/Drive.kt
+++ src/main/kotlin/etc/discount/model/Drive.kt
@@ -6,9 +6,24 @@ import java.time.LocalDateTime
  * 走行データ
  */
 class Drive constructor(
+    /**
+     * 料金所に入った時間
+     */
     val admissionAt: LocalDateTime,
+    /**
+     * 料金所から出た時間
+     */
     val exitAt: LocalDateTime,
+    /**
+     * [CarModel]
+     */
     val model: CarModel,
+    /**
+     * [Route]
+     */
     val route: Route,
+    /**
+     * 1ヶ月の走行回数
+     */
     val count: Long
 )
