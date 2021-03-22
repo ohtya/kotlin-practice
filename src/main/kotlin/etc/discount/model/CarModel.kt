906149574e6e706a8f7789cd9cd0402357d760a7
ohtya <48324058+ohtya@users.noreply.github.com>
2021-03-22 23:58:50 +0900

    feat: 車種を追加

---
+++ src/main/kotlin/etc/discount/model/CarModel.kt
@@ -0,0 +1,33 @@
+package etc.discount.model
+
+/**
+ * 車種（料金車種区分）
+ */
+enum class CarModel {
+
+    /**
+     * 軽自動車等
+     */
+    KEI,
+
+    /**
+     * 普通車
+     */
+    ORDINARY,
+
+    /**
+     * 中型車
+     */
+    MEDIUM,
+
+    /**
+     * 大型車
+     */
+    LARGE,
+
+    /**
+     * 特大車
+     */
+    EXTRA
+
+}
